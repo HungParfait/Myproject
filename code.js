f5e623438d45209f10d0e5f38fd987cb2655a716
HungParfait <81432965+HungParfait@users.noreply.github.com>
2021-03-27 21:39:52 +0700

    Update code.js

--- code.js
+++ code.js
@@ -5,8 +5,8 @@ window.addEventListener('load', () => {
         navigator.location.getCurrentPosition(position => {
             long = positon.coords.latitude;
             lat = position.coords.latitude;
-            const proxy = ``
-            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={9595c3efc7ed6ecc07b163d745519fc9}`;
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={9595c3efc7ed6ecc07b163d745519fc9}`;
             
             
         })
