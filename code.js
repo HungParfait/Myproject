8aed57b7e5c1bdb8edd2880e292df2aa51896147
HungParfait <81432965+HungParfait@users.noreply.github.com>
2021-03-27 17:18:59 +0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    if(navigation.location) {
+        navigator.location.getCurrentPosition(position => {
+            long = positon.coords.latitude;
+            lat = position.coords.latitude;
+            const proxy = ``
+            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={9595c3efc7ed6ecc07b163d745519fc9}`;
+            
+            
+        })
+    }else {
+
+    }
+})
