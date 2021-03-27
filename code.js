e3feaafee4246b06448913267a5ce724202316a2
HungParfait <81432965+HungParfait@users.noreply.github.com>
2021-03-27 22:15:09 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,24 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
-    if(navigation.location) {
-        navigator.location.getCurrentPosition(position => {
-            long = positon.coords.latitude;
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.latitude;
             lat = position.coords.latitude;
             const proxy = `https://cors-anywhere.herokuapp.com/`;
             const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={9595c3efc7ed6ecc07b163d745519fc9}`;
             
+            fetch(api)
+              .then(response => {
+                return response.json()
+              })
+              .then(data => {
+                  console.log(data);
+              });
             
         })
     }else {
+        console.log("cannot define location");
 
     }
-})
+});
